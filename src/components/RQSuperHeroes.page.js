c53bf72284cae131ba512ae1426ad4610e3c3cd2
Rafi340 <rafisamnan340@gmail.com>
2023-05-29 02:02:55 +0600

    update data

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,12 +1,59 @@
-import {useSuperFakeHerosData } from '../hooks/useSuperFakeHerosData';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useEntryData, useSuperFakeHerosData } from '../hooks/useSuperFakeHerosData';
 export const RQSuperHeroesPage = () => {
   const onSuccess = (data) => {}
   const onError = (error) => {}
-const {isLoading , isFetched , data, isError , error } =  useSuperFakeHerosData(onSuccess,onError);
+  const [division_id, setDivision_id] = useState('');
+  console.log(division_id)
+  const [district_id, setDistrict_id] = useState('');
+  const [upazila, setUpazila] = useState('');
+  const [postOffice, setPostOffice] = useState('');
+  const [postCode, setPostCode] = useState('');
+  const [label, setLabel] = useState('');
+const {isLoading , data, isError , error,refetch } =  useSuperFakeHerosData(onSuccess,onError);
+const {mutate: entryInfo} = useEntryData();
+const dataentry = () => {
+  const add = {division_id,district_id, upazila,postOffice,postCode,label};
+  entryInfo(add);
+}
  if(isLoading ) return <h2>Loading...</h2>
   return <>
   <h2>Super Fake hero</h2>
+  <div className='row'>
+    <input type='text'
+    label="division"
+    value={division_id}
+    onChange={(e)=> setDivision_id(e.target.value)}/>
+
+<input type='text'
+  label="district"
+    value={district_id}
+    onChange={(e)=> setDistrict_id(e.target.value)}/>
+
+<input type='text'
+label="upazila"
+    value={upazila}
+    onChange={(e)=> setUpazila(e.target.value)}/>
+
+<input type='text'
+label="PostOffice"
+    value={postOffice}
+    onChange={(e)=> setPostOffice(e.target.value)}/>
+
+<input type='text'
+label="PostOffice"
+    value={postCode}
+    onChange={(e)=> setPostCode(e.target.value)}/>
+
+<input type='text'
+label="label"
+    value={label}
+    onChange={(e)=> setLabel(e.target.value)}/>
+
+<button onClick={dataentry}>Add New</button>
+
+  </div>
   {
   data?.data.map(hero => 
   <div key={hero.id}>
--- src/hooks/useSuperFakeHerosData.js
+++ src/hooks/useSuperFakeHerosData.js
@@ -1,9 +1,12 @@
-import { useQuery } from "react-query";
 import axios from "axios";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 
 const fetchLowHero = () => {
     return axios.get('http://localhost:4000/superheroes');
   };
+  const useAddData = (info) => {
+    return axios.post('http://localhost:4000/superheroes',info);
+  }
 export const useSuperFakeHerosData = (onSuccess,onError) => {
     return useQuery("super-heroes", fetchLowHero,{
         onSuccess,
@@ -11,6 +14,22 @@ export const useSuperFakeHerosData = (onSuccess,onError) => {
         select: (data) => {
             const heroname = data.data.map(hero => hero.name);
             return heroname;
-        } */
+        } */,
+
     },);
+}
+export const useEntryData = () => {
+    const queryClient = useQueryClient();
+    return useMutation(useAddData,
+        {
+            onSuccess: (data)=> {
+                queryClient.setQueryData("super-heroes",(oldQueryData)=> {
+
+                    return {
+                        ...oldQueryData,
+                        data: [data.data,...oldQueryData.data]
+                    }
+                })
+            }
+        })
 }
